1dad96736dd48ee504e5fabaaf0d71bdec4b8459
Adityac115 <adityakushwahaiffco@gmail.com>
2022-12-05 19:20:29 +0530

    prime number checker

---
+++ check_prime.py
@@ -0,0 +1,5 @@
+num=int(input('Enter the number you want to check whether its prime or not: '))
+x=[x for x in range(2,num) if num%x==0] 
+if num==0 or num==1: print(num,"is not a prime number")
+elif len(x)==0: print(num,"is a prime number!!")
+else: print(num,"is not a prime number!!")
