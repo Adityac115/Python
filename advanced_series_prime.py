40444c7b8fd51dd4973b2242a5e43ea737359604
Adityac115 <adityakushwahaiffco@gmail.com>
2022-12-24 03:54:08 +0530

    modifying it

--- advanced_series_prime.py
+++ advanced_series_prime.py
@@ -10,5 +10,7 @@ for x in range(2,limit+1):
         print(x)                                                                  list_values.append(x%n)
 
              
-
+this one checks if there is 0 so
+ dont print number becuase it has factors available
+                                                                         this list contains the remainder of x/n and if this list contains 0 that means there is a factor of that number is available hence dont print it
 ''' 
